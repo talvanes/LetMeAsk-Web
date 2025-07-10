afb82891c6fc05b1317f98ce6901e70a9dd32910
Talvanes Ferreira de Sousa <contato@talvanes.dev>
2025-07-09 21:07:35 -0300

    feat: 'create rooms' and 'room details'

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import './index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { App } from './app.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: mandatory for React
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
