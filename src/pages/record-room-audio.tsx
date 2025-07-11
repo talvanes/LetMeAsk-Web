58b3deed4ed6650b63a0ab31a4e98e354f1b61b8
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2025-07-11 21:55:58 +0000

    feat: add audio recording feature

---
+++ src/pages/record-room-audio.tsx
@@ -0,0 +1,95 @@
+/** biome-ignore-all lint/suspicious/noConsole: testing audio recording feature */
+import { useRef, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+
+const isRecordingSupported =
+  'mediaDevices' in navigator &&
+  'getUserMedia' in navigator.mediaDevices &&
+  'MediaRecorder' in window
+
+type RoomsParams = {
+  roomId: string
+}
+
+export default function RecordRoomAudio() {
+  const params = useParams<RoomsParams>()
+  const [isRecording, setIsRecording] = useState(false)
+  const recorder = useRef<MediaRecorder | null>(null)
+
+  function stopRecording() {
+    setIsRecording(false)
+
+    if (recorder.current && recorder.current.state !== 'inactive') {
+      recorder.current.stop()
+    }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData()
+
+    formData.append('file', audio, 'audio.webm')
+
+    const response = await fetch(`http://localhost:3333/rooms/${params.roomId}/audio`, {
+      method: 'POST',
+      body: formData,
+    })
+
+    const result = await response.json()
+
+    console.log(result)
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert('Seu navegador não suporta gravação de áudio.')
+      return
+    }
+
+    setIsRecording(true)
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    })
+
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: 64_000,
+    })
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data)
+      }
+    }
+
+    recorder.current.onstart = () => {
+      console.log('Gravação iniciada')
+    }
+
+    recorder.current.onstop = () => {
+      console.log('Gravação encerrada/pausada')
+    }
+
+    recorder.current.start()
+  }
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-3">
+      {isRecording ? (
+        <Button onClick={stopRecording}>Pausar Gravação</Button>
+      ) : (
+        <Button onClick={startRecording}>Gravar Áudio</Button>
+      )}
+      {isRecording ? <p>Gravando...</p> : <p>Pausado</p>}
+    </div>
+  )
+}
